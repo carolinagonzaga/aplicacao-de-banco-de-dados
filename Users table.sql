53f2351e5ad322f5bdb602475a81f974d805fd32
Carolina Gonzaga <109047947+carolinagonzaga@users.noreply.github.com>
2026-03-30 22:09:26 -0300

    Database Application course
    Table created in lesson 06 on March 30, 2026, within the Database Application course of QualificaSP through FAT.

---
+++ Users table.sql
@@ -0,0 +1,55 @@
+INSERT INTO DB_T2_CAROLINA_GONZAGA_COSTA.Users (Name) VALUES
+	 ('Tom Hanks'),
+	 ('Oprah Winfrey'),
+	 ('Beyonce'),
+	 ('Julia Roberts'),
+	 ('Pele'),
+	 ('Meryl Streep'),
+	 ('Robin Williams'),
+	 ('Steve Martin'),
+	 ('Jim Carrey'),
+	 ('Michael J Fox');
+INSERT INTO DB_T2_CAROLINA_GONZAGA_COSTA.Users (Name) VALUES
+	 ('Lady Gaga'),
+	 ('Bruno Mars'),
+	 ('Katy Perry'),
+	 ('Adele'),
+	 ('Drake'),
+	 ('Jamie Foxx'),
+	 ('Lizzo'),
+	 ('Halsey'),
+	 ('Lorde'),
+	 ('Zendaya');
+INSERT INTO DB_T2_CAROLINA_GONZAGA_COSTA.Users (Name) VALUES
+	 ('Brad Pitt'),
+	 ('George Clooney'),
+	 ('Gisele Bundchen'),
+	 ('Donald Trump'),
+	 ('Cristiano Ronaldo'),
+	 ('David Beckham'),
+	 ('Bill Gates'),
+	 ('Elvis Presley'),
+	 ('Marilyn Monroe'),
+	 ('Meg Ryan');
+INSERT INTO DB_T2_CAROLINA_GONZAGA_COSTA.Users (Name) VALUES
+	 ('Will Smith'),
+	 ('Tom Cruise'),
+	 ('Angelina Jolie'),
+	 ('Leonardo DiCaprio'),
+	 ('Scarlett Johansson'),
+	 ('Rihanna'),
+	 ('Taylor Swift'),
+	 ('Justin Bieber'),
+	 ('Selena Gomez'),
+	 ('Ariana Grande');
+INSERT INTO DB_T2_CAROLINA_GONZAGA_COSTA.Users (Name) VALUES
+	 ('Jennifer Aniston'),
+	 ('Dwayne Johnson'),
+	 ('Kevin Hart'),
+	 ('Ellen DeGeneres'),
+	 ('Morgan Freeman'),
+	 ('Samuel L Jackson'),
+	 ('Harrison Ford'),
+	 ('Arnold Schwarzenegger'),
+	 ('Sylvester Stallone'),
+	 ('Jackie Chan');
